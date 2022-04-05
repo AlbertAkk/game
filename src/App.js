ad9ee4262b240178ee2605c7efe8d629011e20aa
AlbertAkk <all188@mail.ru>
2022-04-05 19:54:03 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Border from "./components/Border";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Game</h1>
+      <h2>qweqw</h2>
+      <Border />
+    </div>
+  );
+}
+
+export default App;
